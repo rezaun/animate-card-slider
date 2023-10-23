15c7e1a9f68cf44b51811f1120d2a20cc3ebf4fe
Rezaun Kabir <rezaun.kabir@gmail.com>
2023-10-24 04:47:40 +0600

    plugin job has been done

--- animate-card-slider.php
+++ animate-card-slider.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name:Amimate Scroll To Top WP
-Plugin URI :http://wordpress.org/plugins/amimate-scroll-to-top-wp/
-Description: Simple Animate Scroll to Top Wordpress. Easy to manage.
+Plugin Name:Amimate Card Slider 
+Plugin URI :http://wordpress.org/plugins/animate-card-slider
+Description: Animate Card Slider. Slide your anytype of card
 Author: Rezaun Kabir
 Version: 1.0
 Author URI: https://rezaun.netlify.app/
@@ -14,3 +14,78 @@ License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: astt 
 */
+function card_slider_register_cpt(){
+    $labels=[
+        'name' =>'Card Slider'
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => false,
+        'show_ui' => true,
+        'supports' =>['title', 'editor','thumbnail', 'page-attributes']
+    ];
+
+register_post_type('card-slider', $args);    
+}
+add_action('init','card_slider_register_cpt');
+
+
+
+function card_slider_shortcode(){
+    $args = [
+        'post_type' =>'card-slider',
+        'posts_per_page' => -1
+    ];
+
+    $query = new WP_Query($args);
+    
+    $html ='
+    <script>
+    jQuery(document).ready(function($){
+        $(".custom-carousel").owlCarousel({
+            autoWidth: true,
+            loop: true
+          });
+          $(document).ready(function () {
+            $(".custom-carousel .item").click(function () {
+              $(".custom-carousel .item").not($(this)).removeClass("active");
+              $(this).toggleClass("active");
+            });
+          });
+    });
+    </script>
+    
+    <div class="game-section">
+    <div class="owl-carousel custom-carousel owl-theme">';
+
+    while($query->have_posts()) : $query->the_post();
+
+    $html .='<div class="item" style="background-image: url('.get_the_post_thumbnail_url(get_the_ID(), 'large').')">
+      <div class="item-desc">
+        <h3>'.get_the_title().'</h3>
+        '.wpautop(get_the_content()).'
+      </div>
+    </div>';
+
+    endwhile; wp_reset_query();
+
+    $html .= '
+    </div>
+    </div>
+    ';
+
+    return $html;
+
+}
+add_shortcode('card_slider', 'card_slider_shortcode');
+
+
+function card_slider_plugin_assets(){
+
+    $plugin_dir_url = plugin_dir_url(__FILE__);
+
+    wp_enqueue_style('acs-style', $plugin_dir_url.'asset/css/style.css');
+    wp_enqueue_script('owl-carousel', $plugin_dir_url.'asset/js/owl.carousel.min.js', ['jquery'], '1.0.0',true);
+}
+add_action('wp_enqueue_scripts', 'card_slider_plugin_assets');
