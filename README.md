76c9367cd7f3a644fd1b31cd3e67d493740b6e99
Rezaun Kabir <rezaun.kabir@gmail.com>
2023-10-24 03:14:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# animate-card-slider
---
+++ animate-card-slider.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name:Amimate Scroll To Top WP
+Plugin URI :http://wordpress.org/plugins/amimate-scroll-to-top-wp/
+Description: Simple Animate Scroll to Top Wordpress. Easy to manage.
+Author: Rezaun Kabir
+Version: 1.0
+Author URI: https://rezaun.netlify.app/
+License: GPLv2 or later
+Requires at least: 5.2
+Requires PHP: 7.2
+Stable tag: 3.1.6
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: astt 
+*/
